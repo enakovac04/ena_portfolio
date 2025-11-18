ee6ccd09487d8431dad255fc7f15c2c6f214c5d0
Ena Kovac <ehk75@cornell.edu>
2025-11-18 12:29:07 -0500

    changed home page

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ena Kovac — CS & AI Student';
+export const SITE_TITLE = 'Ena Kovac';
 export const SITE_DESCRIPTION = 'Software Development Engineer at Amazon Web Services. Cornell Engineering Computer Science major with a minor in Artificial Intelligence.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
