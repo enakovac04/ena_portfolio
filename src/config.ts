8b551cbf0abce5362e3f2a37fe7cfb474a78c5de
Ena Kovac <ehk75@cornell.edu>
2025-11-19 15:51:19 -0500

    enakovac04 email change

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,4 @@ export const SITE_TITLE = 'Ena Kovac';
 export const SITE_DESCRIPTION = 'Software Development Engineer at Amazon Web Services. Cornell Engineering Computer Science major with a minor in Artificial Intelligence.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
+export const CONTACT_EMAIL = 'enakovac04@gmail.com';
